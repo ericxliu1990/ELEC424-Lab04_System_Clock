c1dd70ac3b84cd7e481433d48ee65cb20c21a4b9
Eric Liu <liuxue1990@gmail.com>
2014-09-25 20:01:17 -0500

    finsih code skeleton

---
+++ blinky.c
@@ -0,0 +1,51 @@
+/*
+A blinky file for Lab03-Blinky, ELEC424 Fall 2014
+Board: Bitcraze 's crazyfle
+Author: Xue Liu, Zichao Wang 
+*/
+
+/* Include files*/
+#include "stm32f10x.h"
+
+/*Public functions*/
+
+/* Init a led 
+@param a integer to represent the LED
+@retval none
+*/
+void LED_Init(int led_num)
+{
+	;
+}
+
+/* LED_On
+@brief turn a led on 
+@param the led number
+@retval none
+*/
+void LED_On(int led_num)
+{
+	;
+}
+/* LED_Off
+@brief turn a led off
+@param the led number 
+@retval none
+*/
+void LED_Off(int led_num)
+{
+	;
+}
+
+/*Private functions*/
+
+/* main program
+@param none
+@retval none
+*/
+int main(void)
+{
+	LED_Init();
+	/* this function should never return */
+	return 0;
+}
---
+++ blinky.h
@@ -0,0 +1,11 @@
+/*
+the blinky header file for Lab03-Blinky, ELEC424 Fall 2014
+Board: Bitcraze 's crazyfle
+Author: Xue Liu, Zichao Wang 
+*/
+/* LED definations */
+
+/* Public functions*/
+void LED_Init(int led_num);
+void LED_On(int led_num);
+void LED_Off(int led_num)';
