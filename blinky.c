bb19f644db0c79f62600c249f1950bdda92e9325
Eric Liu <liuxue1990@gmail.com>
2014-09-30 22:45:40 -0500

    finish project

--- blinky.c
+++ blinky.c
@@ -2,39 +2,58 @@
 A blinky file for Lab03-Blinky, ELEC424 Fall 2014
 Board: Bitcraze 's crazyfle
 Author: Xue Liu, Zichao Wang 
+Drive: STM32F10x_StdPeriph_Lib_V3.5.0
 */
 
 /* Include files*/
-#include "stm32f10x.h"
+#include "blinky.h"
 
 /*Public functions*/
 
 /* Init a led 
-@param a integer to represent the LED
+@param none
 @retval none
 */
-void LED_Init(int led_num)
+void LED_Init()
 {
-	;
+	GPIO_InitTypeDef  GPIO_InitStructure;
+
+	/* Enable the GPIO_LED Clock */
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+
+	/* Configure the GPIO_LED pin */
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
 }
 
 /* LED_On
-@brief turn a led on 
-@param the led number
+@brief turn the green led on 
+@param none
 @retval none
 */
-void LED_On(int led_num)
+void LED_On()
 {
-	;
+	GPIOB->BRR = GPIO_Pin_5;
 }
 /* LED_Off
-@brief turn a led off
-@param the led number 
+@brief turn the green off
+@param none
 @retval none
 */
-void LED_Off(int led_num)
+void LED_Off()
 {
-	;
+	GPIOB->BSRR = GPIO_Pin_5;  
+}
+/* LED_Toggle()
+@param none
+@retval none
+*/
+void LED_Toggle()
+{
+	GPIOB->ODR ^= GPIO_Pin_5;
 }
 
 /*Private functions*/
@@ -46,6 +65,58 @@ void LED_Off(int led_num)
 int main(void)
 {
 	LED_Init();
-	/* this function should never return */
-	return 0;
+	
+	if (SysTick_Config(SystemCoreClock / 1000))
+	{ 
+		/* Capture error */ 
+		while (1);
+	}
+
+	while (1)
+	{
+		LED_Toggle();
+
+		/* Insert 1000 ms delay */
+		_delay_(1000);
+	}
+}
+/* variable defuation for helper functions*/
+static __IO uint32_t Delaytime;
+/* helper functions*/
+/* delay
+@brief: use nop operating to delay for N ms
+@param: nCount for the time need to delay
+@retval: none
+*/
+static void delay(__IO uint32_t nCount)
+{
+	__IO uint32_t index = 0; 
+	for(index = (7500 * nCount); index != 0; index--)
+	{
+	}
+}
+
+/**
+@brief  Inserts a delay time.
+@param  nTime: specifies the delay time, unit(ms).
+@retval None
+  */
+void Delay(__IO uint32_t nTime)
+{ 
+	Delaytime = nTime;
+
+	while(Delaytime != 0);
+}
+
+/**
+@brief  called by systick handler to decreae the counter
+@param  None
+@retval None
+  */
+void Delay_Handler(void)
+{
+	if (Delaytime != 0x00)
+	{ 
+		Delaytime--;
+	}
 }
--- blinky.h
+++ blinky.h
@@ -3,9 +3,41 @@ the blinky header file for Lab03-Blinky, ELEC424 Fall 2014
 Board: Bitcraze 's crazyfle
 Author: Xue Liu, Zichao Wang 
 */
-/* LED definations */
+
+/* LED definations 
+#define LEDn                             2
+
+#define LED_RED_PIN                               GPIO_Pin_4
+#define LED_RED_GPIO_PORT                 GPIOB
+#define LED_RED_GPIO_CLK                    RCC_APB2Periph_GPIOB
+  
+#define LED_GREEN_PIN                         GPIO_Pin_5
+#define LED_GREEN_GPIO_PORT           GPIOB
+#define LED_GREEN_GPIO_CLK              RCC_APB2Periph_GPIOB  
+          
+typedef enum 
+{
+	LED_RED = 0,
+	LED_GREEN = 1
+} Led_TypeDef;
+
+GPIO_TypeDef* LED_PORT[LEDn] = {LED_RED_GPIO_PORT, LED_GREEN_GPIO_PORT,};
+const uint16_t LED_PIN[LEDn] = {LED_RED_PIN, LED_GREEN_PIN};
+const uint32_t LED_CLK[LEDn] = {LED_RED_GPIO_CLK, LED_GREEN_GPIO_CLK};
+*/
+
+/* Delay*/
+#ifndef PROCISE_DELAY
+	#define _delay_ Delay
+#else
+	#define _delay_ delay 
+#endif
 
 /* Public functions*/
-void LED_Init(int led_num);
-void LED_On(int led_num);
-void LED_Off(int led_num)';
+void LED_Init();
+void LED_On();
+void LED_Off();
+void LED_Toggle();
+static void delay(__IO uint32_t nCount);
+void Delay(__IO uint32_t nTime);
+void Delay_Handler(void);
